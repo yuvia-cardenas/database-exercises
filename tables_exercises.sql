b9c97387a166ded4a9117e12e3969efcaf7f0b9b
Yuvia Cardenas <yuvia.cardenas1@gmail.com>
2022-09-07 15:15:48 -0500

    added mysql exercises

---
+++ tables_exercises.sql
@@ -0,0 +1,16 @@
+#3 Use the employees database. Write the SQL code necessary to do this.
+# USE employees;
+#4 List all the tables in the database. Write the SQL code necessary to accomplish this.
+# SHOW tables;
+#5 Explore the employees table. What different data types are present on this table?
+# int, date, varchar, enum
+#6 Which table(s) do you think contain a numeric type column? (Write this question and your answer in a comment)
+# birthday date and hire date
+#7 Which table(s) do you think contain a string type column? (Write this question and your answer in a comment)
+# first and last name
+#8 Which table(s) do you think contain a date type column? (Write this question and your answer in a comment)
+# irthday date and hire date
+#9 What is the relationship between the employees and the departments tables? (Write this question and your answer in a comment)
+# they are in the same database/ schema
+#10 Show the SQL that created the dept_manager table. Write the SQL it takes to show this as your exercise solution.
+# SHOW CREATE TABLE dept_manager;
